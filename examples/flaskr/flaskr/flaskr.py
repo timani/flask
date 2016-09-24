66a2954449ebfedd0b0febe8efe861561a62eb6b
Timani Tunduwani <timani@users.noreply.github.com>
2016-09-24 10:51:42 -0700

    Update flaskr.py

--- examples/flaskr/flaskr/flaskr.py
+++ examples/flaskr/flaskr/flaskr.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-    Flaskr
+    Business Review Export Tool - BREWT
     ~~~~~~
 
-    A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
+    A tool to create product component and customer reports from a csv export.
 
-    :copyright: (c) 2015 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
 """
 
-import os
-from sqlite3 import dbapi2 as sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
-
+import os, cvs
+from flask import Flask, session, g, url_for, abort, flash
 
 # create our little application :)
 app = Flask(__name__)
@@ -23,12 +17,9 @@ app = Flask(__name__)
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     DEBUG=True,
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
+    DATA_FILE='ticket_export.csv'
 ))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
+app.config.from_envvar('BREWT_SETTINGS', silent=True)
 
 def connect_db():
     """Connects to the specific database."""
@@ -36,21 +27,34 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def parse_data_export():
+    with open('names.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row['first_name'], row['last_name'])
+            """Checkes to see if the customer is a match and adds a row to the report."""
+            if hasattr(row['customer'], app.config['DATABASE'][0]):
+                create_customer_report()
 
-def init_db():
-    """Initializes the database."""
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
+def create_customer_report():
+    """Create a customer report."""
+    with open('names.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+        for row in reader:
+            for customer in app.config['CUSTOMERS']:
+                print(customer['name'])
+                """Checkes to see if the customer is a match and adds a row to the report."""
+                if hasattr(row['customer'], app.config['DATABASE'][0]):
+                    create_customer_report()
+                
+def create_component_report():
+    """Create a component report."""
 
 @app.cli.command('initdb')
 def initdb_command():
     """Creates the database tables."""
     init_db()
     print('Initialized the database.')
-
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -60,51 +64,8 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
-
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
-
-
-@app.route('/')
-def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
-
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
